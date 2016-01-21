457656c1f0db2e2a004f6473ec4441942cf658bf
Juan B Cabral <jbc.develop@gmail.com>
2016-01-21 03:44:43 -0300

    end treatment 3

---
+++ treatment_3/views.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+
+from otree.common import Currency as c, currency_range, safe_json
+
+from . import models
+from ._builtin import Page, WaitPage
+from .models import Constants
+
+
+class Instructions(Page):
+
+    def is_displayed(self):
+        return self.subsession.round_number == 1
+
+
+class InstructionsPhase2(Page):
+
+    def is_displayed(self):
+        return self.subsession.round_number == Constants.phase_2_round_start
+
+
+class InstructionsPhase3(Page):
+
+    def is_displayed(self):
+        return self.subsession.round_number == Constants.phase_3_round_start
+
+
+class Question(Page):
+
+    form_model = models.Player
+    form_fields = ["invest"]
+
+
+class ResultsWaitPage(WaitPage):
+
+    def after_all_players_arrive(self):
+        self.group.set_payoff()
+
+
+class Results(Page):
+
+    def is_displayed(self):
+        return True
+
+
+page_sequence = [
+    Instructions, InstructionsPhase2,  InstructionsPhase3,
+    Question, ResultsWaitPage, Results
+]
